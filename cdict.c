2f0001089409d8d8c20fdd75167df0991f359d49
Leandro Schabarum <leandroschabarum.98@gmail.com>
2021-08-08 01:11:49 -0300

    Finished set and delete methods

--- cdict.c
+++ cdict.c
@@ -6,11 +6,16 @@
 
 #define KEY_MAX_SIZE 64
 
+typedef struct DictElement
+{
+	int size;
+	void *value;
+} content;
 
 typedef struct Dictionary
 {
-	void *key;
-	void *content;
+	char *key;
+	content *data;
 } dict;
 
 
@@ -31,12 +36,12 @@ dict* newDict(unsigned int dict_size, ...)
 	localDict = (dict *) malloc(sizeof(dict) * dict_size);
 
 	// in case no memory was allocated returns null pointer
-	// if memSafe is true then raises an error
+	// if memSafe is true then raises an error and resume
 	if (! localDict)
 	{
 		if (memSafe)
 		{
-			fprintf(stderr, "[%u] unable to allocate memory for new dictionary\n", dict_size);
+			fprintf(stderr, "unable to allocate memory for new dictionary <%lu bytes>\n", (sizeof(dict) * dict_size));
 			exit(EXIT_FAILURE);
 		}
 
@@ -46,9 +51,9 @@ dict* newDict(unsigned int dict_size, ...)
 	return &localDict[0];
 }
 
-void addDict(dict *ptr_dict, const char *key, void *value)
+void setKey(dict *ptr_dict, const char *key, const void *data, unsigned int data_size)
 {
-	int key_size = 0;
+	unsigned int key_size = 0;
 	char local_key_copy[KEY_MAX_SIZE];
 
 	while (*key != '\0' && key_size < (KEY_MAX_SIZE - 1))
@@ -69,18 +74,41 @@ void addDict(dict *ptr_dict, const char *key, void *value)
 		
 		if (! ptr_dict->key)
 		{
-			fprintf(stderr, "[unsufficient memory] unable to allocate <%d bytes> for <%s>\n", key_size, local_key_copy);
+			fprintf(stderr, "[unsufficient memory] unable to allocate <%u bytes> for <%s>\n", (key_size + 1), local_key_copy);
 			exit(EXIT_FAILURE);
 		}
 		// using strcpy() because local_key_copy
 		// is always null terminated and within
 		// expected maximum size for dictionary key
 		strcpy(ptr_dict->key, local_key_copy);
-		ptr_dict->content = value;
+		
+		// allocates memory for struct
+		// that will hold key entry data
+		ptr_dict->data = (content *) malloc(sizeof(content));
+
+		if (ptr_dict->data)
+		{
+			// clears allocated memory garbage
+			// and sets the data_size argument
+			// as an attribute of that struct
+			memset(ptr_dict->data, 0, sizeof(content));
+			ptr_dict->data->size = data_size;
+			// allocates memory to hold given data
+			// and copy it over to received address
+			ptr_dict->data->value = (void *) malloc(data_size);
+
+			if (! ptr_dict->data)
+			{
+				fprintf(stderr, "[unsufficient memory] unable to allocate <%u bytes> for given data <%p>\n", data_size, data);
+				exit(EXIT_FAILURE);
+			}
+
+			memcpy(ptr_dict->data->value, data, data_size);
+		}
 	}
 }
 
-void remDict(dict *ptr_dict, char *key)
+void delKey(dict *ptr_dict, char *key)
 {
 	int key_size = 0;
 	char local_key_copy[KEY_MAX_SIZE];
@@ -99,9 +127,18 @@ void remDict(dict *ptr_dict, char *key)
 		// increments dictionary pointer
 		// to key hashed index in array
 		ptr_dict += charKeyHash(local_key_copy, 10);
-		ptr_dict->key = 0;
+		// clears stored data and sets
+		// the address to a null pointer
+		// after freeing allocated memory
+		memset(ptr_dict->key, 0, key_size);
 		free(ptr_dict->key);
-		free(ptr_dict->content);
+		ptr_dict->key = 0;
+		
+		memset(ptr_dict->data->value, 0, ptr_dict->data->size);
+		free(ptr_dict->data->value);
+		ptr_dict->data->size = 0;
+		free(ptr_dict->data);
+		ptr_dict->data = 0;
 	}
 }
 
@@ -114,16 +151,19 @@ int main(void)
 	//char *ptr_userKey = 0;
 
 	dict *d = newDict(10, 1);
-	printf(">>> dict : %lu bytes\n", 10*sizeof(*d));
+	printf(">>> dict : %lu bytes\n\n", 10*sizeof(*d));
+
 	int test = 27;
 
-	addDict(d, "0123456789abcdef", &test);
-	// remDict(d, "0123456789abcdef");
+	setKey(d, "test", &test, sizeof(test));
+	setKey(d, "abc", &test, sizeof(test));
+	delKey(d, "test");
 
 	for (int i = 0; i < 10; i++)
 	{
-		printf("[%d] %s : %p\n", i, (char *) d->key, d->content);
-		printf("\t>>> %d\n", (d->content) ? *((int *) d->content) : 0);
+		printf("%d: (%s) [%p]\n", i, (char *) d->key, d->data);
+		printf("\t%d bytes -> %p", ((d->data) ? d->data->size : 0), ((d->data) ? d->data->value : 0));
+		printf(" -> %d\n", (d->data) ? *((int *) d->data->value) : 0);
 		d++;
 	}
 

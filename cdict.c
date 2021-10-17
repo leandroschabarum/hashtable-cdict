760a06f78ba78ba6e6c09a462a93676debc8b2f8
Leandro Schabarum <leandroschabarum.98@gmail.com>
2021-10-16 22:34:07 -0300

    Fixed delKey() function and adjusted getKey()

--- cdict.c
+++ cdict.c
@@ -58,7 +58,7 @@ dict* newDict(unsigned int dict_size, ...)
 
 	// in case no memory was allocated for
 	// new dictionary returns null pointer
-	// if memSafe is set to true then raises
+	// *if memSafe is set to true then raises
 	// an error and resume program execution
 	if (memSafe)
 	{
@@ -67,6 +67,61 @@ dict* newDict(unsigned int dict_size, ...)
 	}
 
 	return 0;
+}
+
+
+/**
+ * Function for displaying given dictionary entry.
+ *
+ * @param entry *ptr_entry
+ */
+void showEntry(entry *ptr_entry)
+{
+	if (ptr_entry && ptr_entry->key)
+	{
+		printf("# %s => mem(%p) : %d bytes\n", ptr_entry->key, ptr_entry->data, ptr_entry->size);
+	}
+	else
+	{
+		printf("# NULL\n");
+	}
+}
+
+
+/**
+ * Function for displaying all dictionary entries.
+ *
+ * @param dict *ptr_dict
+ */
+void showDict(dict *ptr_dict)
+{
+	if (ptr_dict)
+	{
+		int i = 0;
+		entry *linked_entry = 0;
+
+		if (ptr_dict->entries)
+		{
+			printf("\n>>> BEGIN");
+			entry *ptr_entry = ptr_dict->entries;
+
+			for (i = 0; i < ptr_dict->length; i++)
+			{
+				linked_entry = ptr_entry;
+				printf("\n@ %d\n", i); 
+
+				while (linked_entry)
+				{
+					showEntry(linked_entry);
+					linked_entry = linked_entry->next;
+				}
+	
+				ptr_entry++;
+			}
+
+			printf("<<< END\n");
+		}
+	}
 }
 
 
@@ -90,6 +145,7 @@ void setKey(dict *ptr_dict, const char *key, const void *data, unsigned int data
 		key_size++;
 		key++;
 	}
+
 	// enforces key to always be null terminated
 	local_key_copy[key_size] = '\0';
 
@@ -130,6 +186,7 @@ void setKey(dict *ptr_dict, const char *key, const void *data, unsigned int data
 			fprintf(stderr, "[unsufficient memory] unable to allocate <%u bytes> for key <%s>\n", (key_size + 1), local_key_copy);
 			exit(EXIT_FAILURE);
 		}
+
 		// using strcpy() because local_key_copy
 		// is always null terminated and within
 		// expected maximum size for dictionary key
@@ -141,6 +198,7 @@ void setKey(dict *ptr_dict, const char *key, const void *data, unsigned int data
 			fprintf(stderr, "[unsufficient memory] unable to allocate <%u bytes> for data at <%p>\n", data_size, data);
 			exit(EXIT_FAILURE);
 		}
+
 		// sets the data_size argument
 		// as an attribute of dict entry 
 		ptr_entry->size = data_size;
@@ -167,6 +225,7 @@ void delKey(dict *ptr_dict, char *key)
 		key_size++;
 		key++;
 	}
+
 	// enforces key to always be null terminated
 	local_key_copy[key_size] = '\0';
 
@@ -174,60 +233,105 @@ void delKey(dict *ptr_dict, char *key)
 	
 	if (key_size > 0)
 	{
-		entry *previous = 0;
+		entry *tmp_entry = 0;
+
 		// increments dictionary pointer
 		// to key hashed index in array
 		ptr_entry += charKeyHash(local_key_copy, ptr_dict->length);
 
-		// when entry is not the given key to delete
+		// when entry is not the given key to delete,
 		// loop through linked list until key is
 		// found or empty pointer is reached
-		while (! strcmp(local_key_copy, ptr_entry->key) && ! ptr_entry->next)
+		while (strcmp(local_key_copy, ptr_entry->key) != 0)
 		{
-			previous = ptr_entry;
-			ptr_entry = ptr_entry->next;
+			tmp_entry = ptr_entry;
+
+			if (ptr_entry->next)
+			{
+				ptr_entry = ptr_entry->next;
+				continue;
+			}
+
+			break;
 		}
 
-		printf(">>> %s : %p\n", ptr_entry->key, ptr_entry->data);
-
+		showEntry(ptr_entry); // DEBUG
+		
 		// clears stored data and sets
 		// the address to a null pointer
 		// after freeing allocated memory
-		if (previous)
+		if (tmp_entry)
 		{
+			// case when there is a previous entry to the
+			// mapped key, then tmp_entry points to it
+			tmp_entry->next = ptr_entry->next;
+			ptr_entry->next = 0;
+
 			memset(ptr_entry->key, 0, key_size);
 			free(ptr_entry->key);
 			ptr_entry->key = 0;
-		
+
 			memset(ptr_entry->data, 0, ptr_entry->size);
-			ptr_entry->size = 0;
 			free(ptr_entry->data);
 			ptr_entry->data = 0;
+			ptr_entry->size = 0;
 
-			previous->next = ptr_entry->next;
-			ptr_entry->next = 0;
 			free(ptr_entry);
+			ptr_entry = 0;
 		}
 		else
 		{
-			previous = ptr_entry->next;
-			memset(ptr_entry, 0, sizeof(entry));
+			// case when there is no previous entry to the mapped
+			// key, then tmp_entry points to the next one
+			tmp_entry = ptr_entry->next;
+			ptr_entry->next = 0;
 
-			if (previous->next)
+			memset(ptr_entry->key, 0, key_size);
+			free(ptr_entry->key);
+			ptr_entry->key = 0;
+
+			memset(ptr_entry->data, 0, ptr_entry->size);
+			free(ptr_entry->data);
+			ptr_entry->data = 0;
+			ptr_entry->size = 0;
+
+			// checks if next entry exists and if so,
+			// copies next entry data over to current one
+			if (tmp_entry)
 			{
-				memcpy(ptr_entry, previous, sizeof(entry));
+				ptr_entry->key = (char *) malloc(strlen(tmp_entry->key) + 1);
 
-				memset(previous->key, 0, key_size);
-				free(previous->key);
-				previous->key = 0;
-		
-				memset(previous->data, 0, previous->size);
-				previous->size = 0;
-				free(previous->data);
-				previous->data = 0;
+				if (! ptr_entry->key)
+				{
+					fprintf(stderr, "[unsufficient memory] unable to allocate <%lu bytes> for key <%s>\n", (strlen(tmp_entry->key) + 1), tmp_entry->key);
+					exit(EXIT_FAILURE);
+				}
 
-				previous->next = 0;
-				free(previous);
+				strcpy(ptr_entry->key, tmp_entry->key);
+				memset(tmp_entry->key, 0, strlen(ptr_entry->key));
+				free(tmp_entry->key);
+				tmp_entry->key = 0;
+				
+				ptr_entry->data = (void *) malloc(tmp_entry->size);
+
+				if (! ptr_entry->data)
+				{
+					fprintf(stderr, "[unsufficient memory] unable to allocate <%u bytes> for data at <%p>\n", tmp_entry->size, tmp_entry->data);
+					exit(EXIT_FAILURE);
+				}
+
+				memcpy(ptr_entry->data, tmp_entry->data, tmp_entry->size);
+				ptr_entry->size = tmp_entry->size;
+				memset(tmp_entry->data, 0, ptr_entry->size);
+				free(tmp_entry->data);
+				tmp_entry->data = 0;
+				tmp_entry->size = 0;
+				
+				ptr_entry->next = tmp_entry->next;
+				tmp_entry->next = 0;
+			
+				free(tmp_entry);
+				tmp_entry = 0;
 			}
 		}
 	}
@@ -252,6 +356,7 @@ void* getKey(dict *ptr_dict, const char *key)
 		key_size++;
 		key++;
 	}
+
 	// enforces key to always be null terminated
 	local_key_copy[key_size] = '\0';
 
@@ -261,88 +366,57 @@ void* getKey(dict *ptr_dict, const char *key)
 	{
 		ptr_entry += charKeyHash(local_key_copy, ptr_dict->length);
 
-		return ptr_entry->data;
+		// when entry is not the given key, loop
+		// through linked list until key is
+		// found or empty pointer is reached
+		while (strcmp(local_key_copy, ptr_entry->key) != 0)
+		{
+			if (ptr_entry->next)
+			{
+				ptr_entry = ptr_entry->next;
+				continue;
+			}
+
+			break;
+		}
+
+		showEntry(ptr_entry); // DEBUG
+
+		return (ptr_entry ? ptr_entry->data : 0);
 	}
 
 	return 0;
 }
 
 
-void showDict(dict *ptr_dict)
-{
-	if (ptr_dict)
-	{
-		int i = 0;
-		entry *linked_entry = 0;
-
-		if (ptr_dict->entries)
-		{
-			printf("\n>>> BEGIN");
-			entry *ptr_entry = ptr_dict->entries;
-
-			for (i = 0; i < ptr_dict->length; i++)
-			{
-				linked_entry = ptr_entry;
-				printf("\n#%d: %s => mem(%p)\n", i, linked_entry->key, linked_entry->data);
-				while (linked_entry)
-				{
-					if (linked_entry->data)
-					{
-						printf("|> %s mem(%p) : %d bytes", linked_entry->key, linked_entry->data, linked_entry->size);
-						//printf("\t next %p\n", linked_entry->next);
-						printf("\t%d\n", *((int *) linked_entry->data));
-					}
-
-					linked_entry = linked_entry->next;
-				}
-	
-				ptr_entry++;
-			}
-
-			printf("<<< END\n");
-		}
-	}
-}
-
-
-
 int main(void)
 {
-	//unsigned int newHash;
-	//char userKey[128];
-	//char *ptr_userKey = 0;
+	dict *d = newDict(5, 1);
 
-	dict *d = newDict(10, 1);
+	int test_int = 123;
+	float test_float = 12.345;
+	char test_char[8] = "testing";
 
-	int test = 27;
-
-	setKey(d, "test", &test, sizeof(test));
-	setKey(d, "abc", &test, sizeof(test));
-	//showDict(d);
-
-	setKey(d, "cba", &test, sizeof(test));
-	delKey(d, "test");
+	printf("adding keys...\n");
+	setKey(d, "int", &test_int, sizeof(test_int));
+	setKey(d, "char", &test_char[0], strlen(test_char));
+	setKey(d, "int2del", &test_int, sizeof(test_int));
+	setKey(d, "float", &test_float, sizeof(test_float));
 	showDict(d);
 
+	printf("\ndeleting keys...\n");
+	delKey(d, "int2del");
+	showDict(d);
+	
+	printf("\nReturning stored values...\n");
+	char *string = getKey(d, "char");
+	printf(">>> %s\n", string);
 
-	//int *v = getKey(d, "abc");
-	//printf("### %d\n", *v);
+	int *integer = getKey(d, "int");
+	printf(">>> %d\n", *integer);
 
-	/*
-	while(1) {
-		printf("enter key value />_ ");
-		scanf("%[^\n]%*c", &userKey[0]);
-		ptr_userKey = &userKey[0];
-
-		if (strcmp(userKey, "break") == 0)
-		{
-			break;
-		}
-
-		newHash = charKeyHash(ptr_userKey, MAX_DICT);
-		printf("%s : %d\n", userKey, newHash);
-	}
-	*/
+	float *decimal = getKey(d, "float");
+	printf(">>> %f\n", *decimal);
 
 	return 0;
 }
